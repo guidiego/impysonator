3aed9c8c21177b2bfbb4b3c0c6280c322f342aef
Guilherme Diego <guidiego.expgames@gmail.com>
2018-03-28 00:16:28 -0300

    Improve: NPL constants

--- npl/contants.py
+++ npl/contants.py
@@ -1,7 +1,8 @@
-from re import compile, DOTALL
+from re import compile
 
 IMPERSONAL_EXPRESSIONS = [
-    compile('(N|(\.|\s)n)ós', DOTALL),
-    compile('(N|(\.|\s)n)oss(a|o)', DOTALL),
-    compile('\w*(e|a)mos(,|\.|\s|$)', DOTALL)
+    compile('[N|(\.|\s)n]ós'),
+    compile('[N|(\.|\s)n]os\s'),
+    compile('[N|(\.|\s)n]oss[a|o]'),
+    compile('\w*[e|a]mos[,|\.|\s|$]')
 ]
