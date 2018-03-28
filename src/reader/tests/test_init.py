937aebc2a79ec268c5307a8108ad550af4179ee8
Guilherme Diego <guidiego.expgames@gmail.com>
2018-03-28 10:23:52 -0300

    Include: Integration Test

---
+++ src/reader/tests/test_init.py
@@ -0,0 +1,17 @@
+from reader import read
+
+def assert_file(file, phrase):
+    assert phrase in file.get_body()[0]
+
+def test_read_folder():
+    files = read('__mocks__')
+
+    assert len(files) == 2
+    assert_file(files[0], 'Nossa senhora nosso')
+    assert_file(files[1], 'mais algum texto')
+
+def test_read_file():
+    files = read('__mocks__/file.txt')
+
+    assert len(files) == 1
+    assert_file(files[0], 'Nossa senhora nosso')
