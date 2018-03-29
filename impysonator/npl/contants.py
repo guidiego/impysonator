928f44778a18b93400143522787352b8fc5d4d40
Guilherme Diego <guidiego.expgames@gmail.com>
2018-03-29 20:52:00 -0300

    Fix: Nos regex

--- impysonator/npl/contants.py
+++ impysonator/npl/contants.py
@@ -2,7 +2,7 @@ from re import compile
 
 IMPERSONAL_EXPRESSIONS = [
     compile('^(\.*)nós[\s|\.|$]'),
-    compile('[N|(\.|\s)n]os\s'),
+    compile('Nos| nos[\s|$]|-nos[\s|$]'),
     compile('[N|(\.|\s)n]oss[a|o]'),
     compile('\w*[e|a]mos[,|\.|\s|$]')
 ]
