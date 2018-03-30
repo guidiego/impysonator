df5b7a48818ead24ea6ec22b6829c643fab992aa
Guilherme Diego <guidiego.expgames@gmail.com>
2018-03-29 21:21:52 -0300

    Release: 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='impysonator',
-    version='0.1.2',
+    version='0.1.3',
     description='Seja Impessoal!',
     url='http://github.com/guidiego/impysonator',
     author='Guiherme Diego',
